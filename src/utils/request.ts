068d356200629fdb5553d7327187abf4119960cc
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-06-10 19:56:39 +0800

    https

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,10 +1,15 @@
 import axios from 'axios';
+import https from 'https';
 
-const request = axios.create({
-  timeout: 5000,
-  baseURL: process.env.NODE_ENV === 'production'
-    ? 'https://149.28.53.135/api/'
-    : 'https://149.28.53.135/api/'
-});
+const request = axios
+  .create({
+    timeout: 5000,
+    baseURL: process.env.NODE_ENV === 'production'
+      ? 'https://149.28.53.135/api/'
+      : 'https://149.28.53.135/api/',
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false
+    })
+  });
 
 export default request;
