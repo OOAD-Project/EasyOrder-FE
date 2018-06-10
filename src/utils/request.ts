78218c478e0cc34261b6117d302aeb040dfdb154
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-06-10 19:39:09 +0800

    https

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 const request = axios.create({
   timeout: 5000,
   baseURL: process.env.NODE_ENV === 'production'
-    ? 'http://149.28.53.135/api/'
-    : 'http://149.28.53.135/api/'
+    ? 'https://149.28.53.135/api/'
+    : 'https://149.28.53.135/api/'
 });
 
 export default request;
