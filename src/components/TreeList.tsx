bea6d4641d2e5c824d18367784208bc3bbff68c9
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-06-10 17:02:23 +0800

    tag 分类

--- src/components/TreeList.tsx
+++ src/components/TreeList.tsx
@@ -2,7 +2,7 @@ import { ProductType } from '@/stores/products';
 import { List } from 'antd-mobile';
 import { action, computed, observable } from 'mobx';
 import { observer } from 'mobx-react';
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import styles from './TreeList.less';
 
 const Item = List.Item;
@@ -15,19 +15,49 @@ interface ITreeListProps {
 @observer
 export default class TreeList extends React.Component<ITreeListProps> {
 
+  @observable
+  filter: 'all' | number = 'all';
+
+  @computed
+  get data() {
+    const { dataSource } = this.props;
+    return this.filter === 'all'
+      ? dataSource
+      : dataSource.filter(({ tag_id }) => tag_id === this.filter);
+  }
+
   @computed
   get Categories() {
-    const list = this.props.tagIds.map((tagId) => (
+    const { tagIds } = this.props;
+    const base = tagIds.map((tagId) => (
       <Item key={ tagId }>
-        { tagId }
+        <div
+          data-tag={ tagId }
+          className={ styles.tagId }
+          onClick={ this.handleClickFilter }
+        >{ tagId }
+        </div>
       </Item>
     ));
+    const list = [
+      (
+        <Item key={ 'all' }>
+          <div
+            data-tag={ 'all' }
+            className={ styles.tagId }
+            onClick={ this.handleClickFilter }
+          >{ '全部' }
+          </div>
+        </Item>
+      ),
+      ...base
+    ];
     return (<List>{ list }</List>);
   }
 
   @computed
   get Products() {
-    const list = this.props.dataSource.map((good) => {
+    const list = this.data.map((good) => {
       return (
         <Item key={ good.id } align={ 'top' } multipleLine={ true }>
           <div className={ styles.meta }>
@@ -37,7 +67,9 @@ export default class TreeList extends React.Component<ITreeListProps> {
             <div className={ styles.content }>
               <div className={ styles.title }>{ good.name }</div>
               <div className={ styles.brief }>{ good.description }</div>
-              <div className={ styles.brief }>月售{ good.sales_permonth } 赞{ good.likes }</div>
+              <div className={ styles.brief }>
+                月售{ good.sales_permonth } 剩余{ good.amount } 赞{ good.likes }
+              </div>
               <div className={ styles.price_container }>
                 <div className={ styles.price }>{ good.price }</div>
                 <div className={ styles.plus }>+</div>
@@ -48,6 +80,13 @@ export default class TreeList extends React.Component<ITreeListProps> {
       );
     });
     return (<List>{ list }</List>);
+  }
+
+  @action
+  handleClickFilter: MouseEventHandler<HTMLDivElement> = (e) => {
+    if (e.currentTarget.dataset.tag) {
+      this.filter = e.currentTarget.dataset.tag === 'all' ? 'all' : Number.parseInt(e.currentTarget.dataset.tag);
+    }
   }
 
   render() {
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,6 +1,11 @@
 import stores from '@/stores';
+import { configure } from 'mobx';
 import { observer, Provider } from 'mobx-react';
 import React from 'react';
+
+configure({
+  enforceActions: true
+});
 
 @observer
 export default class AppLayout extends React.Component<{}> {
